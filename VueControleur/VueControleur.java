c42f34883a1cfd4531fd252938384a8c84821546
CyprienBonnefond <cypri173@gmail.com>
2026-03-16 10:28:44 +0100

    changement smiley

--- VueControleur/VueControleur.java
+++ VueControleur/VueControleur.java
@@ -37,11 +37,15 @@ public class VueControleur extends JPanel implements Observer {
     private ImageIcon flagIcon;
     private ImageIcon bombIcon;
     private ImageIcon[] numberIcons;
+    private ImageIcon smileyNeutralIcon;
+    private ImageIcon smileyClickIcon;
+    private ImageIcon smileyLoseIcon;
+    private ImageIcon smileyWinIcon;
 
 
     private JLabel compteurMinesLabel;
     private JLabel timerLabel;
-    private SmileyButton smileButton;
+    private JButton smileButton;
 
 
     private Timer timer;
@@ -52,139 +56,31 @@ public class VueControleur extends JPanel implements Observer {
     public enum EtatSmiley { NORMAL, CLIC, PERDU, GAGNE }
 
 
-    
-    private static class SmileyButton extends JButton {
+    private static class HexCellLabel extends JLabel { //représentation case hexagonale
 
+        private final Polygon shape; //definition forme hexagonale
+        private boolean lowered = false; //état de la case (affichage)
 
-        private EtatSmiley etat = EtatSmiley.NORMAL;
-        private static final int TAILLE = 36;
-
-
-        public SmileyButton() {
-            setPreferredSize(new Dimension(TAILLE + 4, TAILLE + 4));
-            setFocusPainted(false);
-            setContentAreaFilled(false);
-            setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
-        }
-
-
-        public void setEtat(EtatSmiley e) {
-            etat = e;
-            repaint();
-        }
-
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-
-            int w = getWidth();
-            int h = getHeight();
-            int d = Math.min(w, h) - 6;
-            int ox = (w - d) / 2;
-            int oy = (h - d) / 2;
-
-
-            g2.setColor(new Color(255, 220, 0));
-            g2.fillOval(ox, oy, d, d);
-
-
-            g2.setColor(new Color(160, 120, 0));
-            g2.setStroke(new BasicStroke(1.5f));
-            g2.drawOval(ox, oy, d, d);
-
-
-            int eyeY = oy + d / 3;
-            int eyeSize = Math.max(2, d / 8);
-            int leftEyeX  = ox + d * 3 / 8 - eyeSize / 2;
-            int rightEyeX = ox + d * 5 / 8 - eyeSize / 2;
-
-
-            if (etat == EtatSmiley.PERDU) {
-                g2.setColor(Color.BLACK);
-                g2.setStroke(new BasicStroke(1.5f));
-                dessinerCroix(g2, leftEyeX,  eyeY, eyeSize);
-                dessinerCroix(g2, rightEyeX, eyeY, eyeSize);
-            } else if (etat == EtatSmiley.CLIC) {
-                g2.setColor(Color.BLACK);
-                g2.drawOval(leftEyeX,  eyeY, eyeSize, eyeSize);
-                g2.drawOval(rightEyeX, eyeY, eyeSize, eyeSize);
-            } else {
-                g2.setColor(Color.BLACK);
-                g2.fillOval(leftEyeX,  eyeY, eyeSize, eyeSize);
-                g2.fillOval(rightEyeX, eyeY, eyeSize, eyeSize);
-            }
-
-
-            if (etat == EtatSmiley.GAGNE) {
-                g2.setColor(new Color(0, 0, 0, 180));
-                int gSize = eyeSize + 4;
-                g2.fillRoundRect(leftEyeX  - 2, eyeY - 1, gSize, gSize - 1, 3, 3);
-                g2.fillRoundRect(rightEyeX - 2, eyeY - 1, gSize, gSize - 1, 3, 3);
-                g2.setColor(Color.BLACK);
-                g2.setStroke(new BasicStroke(1f));
-                g2.drawLine(leftEyeX + gSize - 2, eyeY + gSize / 2,
-                            rightEyeX - 2,        eyeY + gSize / 2);
-            }
-
-
-            g2.setColor(Color.BLACK);
-            g2.setStroke(new BasicStroke(1.8f));
-            int boucheY = oy + d * 3 / 5;
-            int boucheW = d * 2 / 5;
-            int boucheX = ox + (d - boucheW) / 2;
-            int boucheH = d / 5;
-
-
-            if (etat == EtatSmiley.PERDU) {
-                g2.drawArc(boucheX, boucheY, boucheW, boucheH, 0, 180);
-            } else if (etat == EtatSmiley.CLIC) {
-                int r = d / 10;
-                g2.drawOval(ox + d / 2 - r, boucheY, r * 2, r * 2);
-            } else {
-                g2.drawArc(boucheX, boucheY - boucheH / 2, boucheW, boucheH, 180, 180);
-            }
-
-
-            g2.dispose();
-        }
-
-
-        private void dessinerCroix(Graphics2D g2, int x, int y, int size) {
-            g2.drawLine(x, y, x + size, y + size);
-            g2.drawLine(x + size, y, x, y + size);
-        }
-    }
-
-
-    private static class HexCellLabel extends JLabel {
-
-        private final Polygon shape;
-        private boolean lowered = false;
-
-        public HexCellLabel(int width, int height) {
-            super("", SwingConstants.CENTER);
-            shape = creerHexagone(width, height);
+        public HexCellLabel(int width, int height) { //constructeur 
+            super("", SwingConstants.CENTER); 
+            shape = creerHexagone(width, height); //création forme hexagonale
             setPreferredSize(new Dimension(width, height));
             setOpaque(false);
             setBackground(new Color(192, 192, 192));
         }
 
-        public void setLowered(boolean value) {
+        public void setLowered(boolean value) { 
             lowered = value;
-            repaint();
+            repaint(); 
         }
 
         @Override
-        public boolean contains(int x, int y) {
+        public boolean contains(int x, int y) {//permet de détecter les clics uniquement à l'intérieur de l'hexagone
             return shape.contains(x, y);
         }
 
         @Override
-        protected void paintComponent(Graphics g) {
+        protected void paintComponent(Graphics g) { //
             Graphics2D g2 = (Graphics2D) g.create();
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
@@ -250,6 +146,10 @@ public class VueControleur extends JPanel implements Observer {
         flagIcon    = chargerIcone("images/flag.png");
         bombIcon    = chargerIcone("images/bomb.png");
         numberIcons = new ImageIcon[9];
+        smileyNeutralIcon = chargerIcone("images/smiley-neutral.png", 34);
+        smileyClickIcon   = chargerIcone("images/smiley-onclick.png", 34);
+        smileyLoseIcon    = chargerIcone("images/smiley-lose.png", 34);
+        smileyWinIcon     = chargerIcone("images/smiley-win.png", 34);
         for (int i = 0; i <= 8; i++) {
             numberIcons[i] = chargerIcone("images/" + i + ".png");
         }
@@ -268,6 +168,38 @@ public class VueControleur extends JPanel implements Observer {
                 : Math.max(12, pxCase - 10);
         Image scaled = icon.getImage().getScaledInstance(iconSize, iconSize, Image.SCALE_SMOOTH);
         return new ImageIcon(scaled);
+    }
+
+    private ImageIcon chargerIcone(String chemin, int taille) {
+        URL url = getClass().getClassLoader().getResource(chemin);
+        if (url == null) {
+            System.err.println("[WARN] Image introuvable : " + chemin);
+            return null;
+        }
+        ImageIcon icon = new ImageIcon(url);
+        Image scaled = icon.getImage().getScaledInstance(taille, taille, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaled);
+    }
+
+    private void setEtatSmiley(EtatSmiley etat) {
+        if (smileButton == null) {
+            return;
+        }
+        switch (etat) {
+            case CLIC:
+                smileButton.setIcon(smileyClickIcon);
+                break;
+            case PERDU:
+                smileButton.setIcon(smileyLoseIcon);
+                break;
+            case GAGNE:
+                smileButton.setIcon(smileyWinIcon);
+                break;
+            case NORMAL:
+            default:
+                smileButton.setIcon(smileyNeutralIcon);
+                break;
+        }
     }
 
 
@@ -389,7 +321,7 @@ public class VueControleur extends JPanel implements Observer {
                     } else {
                         jlab.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
                     }
-                    smileButton.setEtat(EtatSmiley.CLIC);
+                    setEtatSmiley(EtatSmiley.CLIC);
                 }
             }
 
@@ -404,7 +336,7 @@ public class VueControleur extends JPanel implements Observer {
                     }
                 }
                 if (jeu.isEnCours()) {
-                    smileButton.setEtat(EtatSmiley.NORMAL);
+                    setEtatSmiley(EtatSmiley.NORMAL);
                 }
             }
         });
@@ -426,7 +358,12 @@ public class VueControleur extends JPanel implements Observer {
         panelGauche.add(compteurMinesLabel);
 
 
-        smileButton = new SmileyButton();
+        smileButton = new JButton();
+        smileButton.setPreferredSize(new Dimension(40, 40));
+        smileButton.setFocusPainted(false);
+        smileButton.setContentAreaFilled(false);
+        smileButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        smileButton.setIcon(smileyNeutralIcon);
         smileButton.addActionListener(e -> {
             arreterTimer(); 
             restartAction.run();
@@ -561,10 +498,10 @@ public class VueControleur extends JPanel implements Observer {
 
         if (jeu.isPerdu()) {
             arreterTimer();
-            smileButton.setEtat(EtatSmiley.PERDU);
+            setEtatSmiley(EtatSmiley.PERDU);
         } else if (jeu.isGagne()) {
             arreterTimer();
-            smileButton.setEtat(EtatSmiley.GAGNE);
+            setEtatSmiley(EtatSmiley.GAGNE);
         }
     }
 }
